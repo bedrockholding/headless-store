283f802101582ff432416f2434b4e2eab98842e4
j0hnPrado <jprado@bedrockholding.com>
2026-04-16 21:34:14 +0800

    add-start-earning-button

--- src/components/progress-amount-client.tsx
+++ src/components/progress-amount-client.tsx
@@ -4,8 +4,21 @@ import { ProgressAmount, useGameApi } from "getjacked-components";
 
 export function ProgressAmountClient() {
 
-  const partnerCode = "jmdeleon";
-  const { rewardAmount } = useGameApi(partnerCode, "");
+  const partnerCode = "storefront";
+  const { rewardAmount, sessionUser } = useGameApi(partnerCode, "");
 
-  return <ProgressAmount amount={rewardAmount ? rewardAmount.toString() : "0"} />;
+  if (!sessionUser.email) {
+    return (
+    <>
+      <a href="/rewards" className="text-[14px] font-bold text-white bg-black hover:opacity-90 py-2.5 p-3 hidden sm:inline-flex items-center">
+        Start Earning Now
+      </a>
+      <div className="inline-flex sm:hidden"><ProgressAmount amount={rewardAmount ? rewardAmount.toString() : "0"} /></div>
+    </>
+    )
+  }else {
+    return <ProgressAmount amount={rewardAmount ? rewardAmount.toString() : "0"} />;
+  }
+
+
 }
